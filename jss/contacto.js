0b2695dba0dcadc3a13a24b0361994d0a293f532
Graileth Pereira <grailethpereirasanm@gmail.com>
2023-08-04 05:18:56 -0400

    carrito de compras

---
+++ jss/contacto.js
@@ -0,0 +1,6 @@
+const env = document.querySelector("#env");
+env.addEventListener("click", mostrarAlerta);
+
+function mostrarAlerta() {
+    alert("Tu formulario ha sido enviado con éxito");
+}
--- jss/main.js
+++ jss/main.js
@@ -1,5 +1,4 @@
-
 let usuario = prompt('Ingrese nombre de usuario');
+localStorage.setItem("usuario-ingresado", usuario);
 let saludo = document.getElementById('hola');
 saludo.innerHTML = `<h1>Hola ${usuario}</h1>`;
-console.log(saludo.innerHTML);
