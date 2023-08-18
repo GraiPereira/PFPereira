a00fe149e302f89c895fe2bdbe0cd4f9c143e847
Graileth Pereira <grailethpereirasanm@gmail.com>
2023-08-18 00:29:11 -0400

    Finalizando entrega del proyecto

--- jss/servicios.js
+++ jss/servicios.js
@@ -1,16 +1,28 @@
-const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13';
+const resultElement = document.getElementById('result');
+
+async function translateText() {
+const url = 'https://text-translator2.p.rapidapi.com/translate';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'd7b1fb6ca8msh05b66b74da6f812p1daa04jsna547c25aaf25',
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-	}
+    method: 'POST',
+    headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'X-RapidAPI-Key': 'd7b1fb6ca8msh05b66b74da6f812p1daa04jsna547c25aaf25',
+    'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+    },
+    body: new URLSearchParams({
+    source_language: 'auto',
+    target_language: 'id',
+    text: 'What is your name?'
+    })
 };
 
 try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
+    const response = await fetch(url, options);
+    const { data } = await response.json();
+    resultElement.innerHTML = data.translations[0].translated_text;
 } catch (error) {
-	console.error(error);
+    resultElement.innerHTML = 'Ocurrió un error al traducir el texto';
 }
+}
+
+translateText();
